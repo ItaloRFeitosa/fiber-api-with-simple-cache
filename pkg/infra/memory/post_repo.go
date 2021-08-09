cac571568cbd4ebe0168020ae8c2d24228aa38c5
Italo Feitosa <italo85199@gmail.com>
2021-08-09 09:09:25 -0300

    refac(infra/memory): rename

--- pkg/infra/memory/post_repo.go
+++ pkg/infra/memory/post_repo.go
@@ -3,6 +3,7 @@ package inmemmory
 import "github.com/italorfeitosa/fiber-api-with-simple-cache/pkg/domain"
 
 type PostRepo struct {
+    mu sync.RWMutex
 	Posts []domain.Post
 }
 
@@ -13,6 +14,9 @@ func NewPostRepo() *PostRepo {
 	}
 }
 
-func (repo *PostRepo) Insert(post domain.Post) {
-	repo.Posts = append(repo.Posts, post)
+func (r *PostRepo) Insert(post domain.Post) error{
+	r.mu.Lock()
+	r.Posts = append(r.Posts, post)
+	r.mu.Unlock()
+	return nil
 }
